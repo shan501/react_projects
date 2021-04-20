93b6b5c49be79e5df062bd2d7300a22921f16b57
shan501 <59776784+shan501@users.noreply.github.com>
2021-04-20 09:22:31 -0400

    Add files via upload

---
+++ setup/src/Tour.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+const Tour = ({id,image,info,name,price,removeTour}) => {
+  const [readMore,setReadMore]=useState(false)
+  return(
+    <article className='single-tour'>
+      <img src={image} alt={name}/>
+      <footer>
+        <div>
+          <h4>{name}</h4>
+          <h4 className="tour-price"></h4>
+        </div>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+          {readMore ? 'show less' : '  read more'}
+          </button>
+        </p>
+        <button className='delete-btn' onClick={()=> removeTour(id)}>
+          Not Interested
+        </button>
+      </footer>
+
+    </article>
+  )
+
+
+
+};
+
+export default Tour;
